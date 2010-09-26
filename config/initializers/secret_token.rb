bb2d5e1816f8c114ab4ee299944427b260d2f5a8
Stephen A. Goss <steveth45@gmail.com>
2010-09-26 11:40:08 -0700

    Empty Rails 3.0 Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MdDocs::Application.config.secret_token = '5ab4ae15ce6fdd1eb794596f08a5d26c1795c5424ca0a499d285003d98afbd0503d3fd5ec8275d660fafc4d644ffe71cf76e0f8645906c994499602098f0b7dc'
